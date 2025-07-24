0ffd7b5df18d8a0cf26bf814e1a13b5853939e84
hellosamrat00 <shahisamrat711@gmail.com>
2025-07-24 22:48:36 +0545

    Refactor index.js to enhance middleware organization and improve code clarity

--- server/index.js
+++ server/index.js
@@ -1,17 +1,23 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
-import connectDB from "./config/Db.js";
+import connectDB from "./config/Db.js"; 
+import { clerkMiddleware } from '@clerk/express'
 import clerkWebhooks from "./controllers/clerkWebHooks.js";
 
+
 connectDB();
+
 const app = express();
 
-app.use(cors());
-app.use(express.json()); // only for normal routes
 
-// raw body middleware only for Clerk webhook route
-app.post("/api/clerk/events", express.raw({ type: "*/*" }), clerkWebhooks);
+app.use(cors());
+app.use(express.json()); 
+app.use(clerkMiddleware())
+
+app.use("/api/clerk",clerkWebhooks)
 
 const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => res.send("API WORKING"));
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
